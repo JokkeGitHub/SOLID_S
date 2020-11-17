fda3cd222b1d97c7d341c4fde8b7a00a453b50bf
Joachim <joac3146@zbc.dk>
2020-11-17 10:58:20 +0100

    Add project files.

---
+++ SOLID_S/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_S
+{
+    public class Person
+    {
+        // Our objects private proterties
+        private string _firstName;
+        private string _lastName;
+
+        // Encapsulation of the private properties
+        #region Encapsulation
+        public string FirstName
+        {
+            get
+            {
+                return this._firstName;
+            }
+            set
+            {
+                this._firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this._lastName;
+            }
+            set
+            {
+                this._lastName = value;
+            }
+        }
+        #endregion
+
+        // Constructors of the object
+        #region Constructors
+        public Person()
+        {
+
+        }
+
+        public Person(string _firstName, string _lastName)
+        {
+            FirstName = _firstName;
+            LastName = _lastName;
+        }
+        #endregion
+    }
+}
---
+++ SOLID_S/PersonDataCapture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_S
+{
+    public class PersonDataCapture
+    {
+        public static Person Capture()
+        {
+            // Ask the user for information
+            Person output = new Person();
+
+            Console.Write("What is your first name? : ");
+            output.FirstName = Console.ReadLine();
+
+            Console.Write("What is your lastname? : ");
+            output.LastName = Console.ReadLine();
+
+            return output;
+        }
+    }
+}
